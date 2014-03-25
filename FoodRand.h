fc22972ae732a8e5044bd0e852842eebc4491396
Petrovich <wallyanastas@gmail.com>
2014-03-25 14:56:13 +1300

    hFile

--- FoodRand.h
+++ FoodRand.h
@@ -2,5 +2,6 @@
 #define FOODRAND_H
 #include"Global.h"
 char FoodRand(int Snake[][2],char Field[][HEIGTH], int Snakesize);
+bool isSnakeBody(int Snake[][2],int x, int y, int Snakesize);
 #endif // FOODRAND_H
 
