38b91aabbdf4f86c203249f030f3a947c19795e9
Petrovich <wallyanastas@gmail.com>
2014-03-25 14:52:12 +1300

    foooooooooooooooood

--- FoodRand.cpp
+++ FoodRand.cpp
@@ -6,21 +6,34 @@
 #include<time.h>
 /*
 
--случайое появление еды на поле
+-случайое появление еды внутри рамок
+(еда не может появиться на рамке ,обрамляющей игровое поле )
+-появление в другом месте , если совпадают координаты головы или туловища, и еды
+-создать цикл, в котором сравниваются координаты еды и змейки
 -
   */
-char FoodRand(int Snake[][2],char Field[][HEIGTH], int Snakesize)
+
+bool isSnakeBody(int Snake[][2],int x, int y, int Snakesize)
+{
+    for(int i=0;i<Snakesize;i++)
+    {
+        if (Snake[i][0]==x&&Snake[i][1]==y) return true ;
+    }
+return  false;
+}
+
+char FoodRand(int Snake[][2],char Field[][HEIGTH],int Snakesize)
 {
     srand(time(NULL));
     int x,y;
     do
     {
-    int x=rand()%WIDTH-1;
-    int y=rand()%HEIGTH-1;
-    }
-    while(x==0||y==0);
+     x=rand()%HEIGTH-1;
+     y=rand()%WIDTH-1;
+    }while(x==0||y==0||isSnakeBody(Snake,x,y,Snakesize));
+
+    Field[y][x]=0xF9;
 
 
-
-    return 0xF9;
+    return 0;
 }
